861b55cb85395930fa5fb4dc51276e451d8d1001
Trinay39 <trinay2004@gmail.com>
2026-05-18 15:43:58 +0530

    Initial travel website setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-blue-500 text-white flex items-center justify-center text-4xl">
+      Tailwind Working!
+    </div>
+  );
+}
